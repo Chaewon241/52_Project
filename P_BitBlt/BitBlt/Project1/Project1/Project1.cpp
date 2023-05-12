1045b538267c858b4a5b7b800d1ca50331fcc662
Chaewon <77910498+Chaewon241@users.noreply.github.com>
2023-05-12 21:51:30 +0900

    c언어 수업때 한거

---
+++ P_BitBlt/BitBlt/Project1/Project1/Project1.cpp
@@ -0,0 +1,121 @@
+﻿// Project1.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+
+#include "framework.h"
+#include "Project1.h"
+
+#pragma comment(lib, "msimg32.lib")		//
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+
+	HDC hMemDC;
+	HBITMAP hImage, hOldBitmap;
+	int bx, by;
+	BITMAP bit;
+
+	switch (message) {
+	case WM_PAINT:
+	{
+		hdc = BeginPaint(hWnd, &ps);
+		hMemDC = CreateCompatibleDC(hdc); // 메모리 DC를 만든다
+
+		// 비트맵 속성으로 파일을 불러오고 HBITMAP으로 형변환해서 hImage에 저장한다
+		// fuLoad 속성: 리소스 대신 파일명으로, 호환 비트맵이 아닌 DIB 섹션 비트맵으로 불러온다
+		hImage = (HBITMAP)LoadImage(NULL, TEXT("image.bmp"),
+			IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
+
+		// hImage가 선택되기 전의 핸들을 저장해 둔다
+		hOldBitmap = (HBITMAP)SelectObject(hMemDC, hImage);
+
+		// 비트맵 정보를 알아낸다
+		GetObject(hImage, sizeof(BITMAP), &bit);
+		bx = bit.bmWidth;
+		by = bit.bmHeight;
+/*
+		unsigned int* p = new unsigned int[bx * 100];
+		for (size_t i = 0; i < bx * 100; i++)
+		{
+			*(p+i) = 0x00FF0000;  //0xAARRGGBB
+		}		
+		// create bitmap
+		{
+			BITMAPINFO BmpInfo;
+			BmpInfo.bmiHeader.biSize = sizeof(BITMAPINFO) - sizeof(RGBQUAD);
+			BmpInfo.bmiHeader.biWidth = bx;
+			BmpInfo.bmiHeader.biHeight = 0 - (int)100; //by
+			BmpInfo.bmiHeader.biPlanes = 1;
+			BmpInfo.bmiHeader.biBitCount = 32;		////0xAARRGGBB
+			BmpInfo.bmiHeader.biCompression = BI_RGB;
+			BmpInfo.bmiHeader.biSizeImage = 0;
+			BmpInfo.bmiHeader.biXPelsPerMeter = 0;
+			BmpInfo.bmiHeader.biYPelsPerMeter = 0;
+			BmpInfo.bmiHeader.biClrUsed = 0;
+			BmpInfo.bmiHeader.biClrImportant = 0;
+
+			SetDIBits(hdc, hImage, 0, 100, p, &BmpInfo, DIB_RGB_COLORS);
+			//SetBitmapBits(hImage, bx * 100 * 4, p);
+		}
+
+		delete[] p;
+*/
+    	// 화면의 (50, 50) 위치에 원본 크기로 원본의 (0, 0) 위치부터 추출하여 그린다
+		BitBlt(hdc, 50, 50, bx, by, hMemDC, 0, 0, SRCCOPY);
+
+    	// RGB(255, 0, 255)(핑크색)을 투명하게 만들고 그린다 //msimg32.lib
+		TransparentBlt(hdc, 350, 50, bx, by, hMemDC, 0, 0, bx, by, RGB(255, 0, 255));
+
+		// 이번에는 왼쪽을 보고 있고 한발을 내딛은 캐릭터(60, 200)를 그린다 
+		TransparentBlt(hdc, 650, 50, (bx / 4) * 1, (by / 4) * 1,
+			hMemDC, 60, 200, bx / 4, by / 4, RGB(255, 0, 255));
+
+		SelectObject(hMemDC, hOldBitmap); // hImage 선택을 해제하기 위해 hOldBitmap을 선택한다
+		DeleteObject(hImage); // 선택 해제된 비트맵을 제거한다
+
+		DeleteDC(hMemDC); // 메모리 DC를 제거한다
+		EndPaint(hWnd, &ps);
+	}
+		return 0;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	WNDCLASS wc;
+
+	wchar_t my_class_name[] = L"MyClassName";
+	wc.cbClsExtra = NULL;
+	wc.cbWndExtra = NULL;
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hInstance = hInstance;
+	wc.lpfnWndProc = WndProc;
+	wc.lpszClassName = my_class_name;
+	wc.lpszMenuName = NULL;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+
+	RegisterClass(&wc);
+
+	HWND hWnd = CreateWindow(my_class_name, L"Test WinAPI", WS_OVERLAPPEDWINDOW,
+		0, 0, 800, 600, NULL, NULL, hInstance, NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+
---
+++ P_WinAPI_anim/P_WinAPI_anim/Project2/Project2/Project2.cpp
@@ -0,0 +1,296 @@
+﻿// Project2.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+
+#include "framework.h"
+#include "Project2.h"
+#define MAX_LOADSTRING 100
+
+#pragma comment(lib, "msimg32.lib")
+
+// 전역 변수:
+HINSTANCE hInst;                                // 현재 인스턴스입니다.
+WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+
+
+SYSTEMTIME st;
+static TCHAR sTime[128] = _T("");
+static RECT rt = { 100,100,400,120 };
+
+static int x = 300, y = 0;
+static int frame = 0;
+
+void Init() 
+{
+}
+void Release() 
+{
+}
+
+void Draw(HDC hdc) 
+{    
+    BITMAP bit; int bx, by;
+
+    HBITMAP MyBitmap1 = 0;
+    HBITMAP MyBitmap2 = 0;
+    HBITMAP MyBitmap3 = 0;
+    HBITMAP MyBitmap4 = 0;
+    MyBitmap1 = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_BITMAP1));    //로딩
+    MyBitmap2 = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_BITMAP2));    //로딩
+    MyBitmap3 = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_BITMAP3));    //로딩
+    MyBitmap4 = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_BITMAP4));    //로딩
+
+    // 비트맵 정보를 알아낸다
+    //GetObject(MyBitmap1, sizeof(BITMAP), &bit);
+    //bx = bit.bmWidth;
+    //by = bit.bmHeight;
+
+    HDC MemDC = 0;
+    HDC MemDC1 = 0;
+    HDC MemDC2 = 0;
+
+    HBITMAP OldBitmap = 0;
+
+    // 더블 버퍼링 -----------------------------------------------------------
+
+    MemDC = CreateCompatibleDC(hdc); 			                            //메모리DC 생성  //배경
+    HBITMAP hBitmap = CreateCompatibleBitmap(hdc, 800, 600);
+    SelectObject(MemDC, hBitmap);                                           //비트맵 선택    
+
+        FillRect(MemDC, &rt, (HBRUSH)GetStockObject(GRAY_BRUSH));               //배경색  //GetClientRect(hWnd, &rt);
+
+        MemDC1 = CreateCompatibleDC(hdc);
+        OldBitmap = (HBITMAP)SelectObject(MemDC1, MyBitmap1); 	                //비트맵 선택    //객체2
+        BitBlt(MemDC, 0, 0, 256, 256, MemDC1, 0, 0, SRCCOPY); 		            //배경에 복사 및 출력
+
+        MemDC2 = CreateCompatibleDC(hdc);
+        SelectObject(MemDC2, MyBitmap2); 	                                    //비트맵 선택    //객체2
+        StretchBlt(MemDC, 200, 0, 128, 128, MemDC2, 0, 0, 256, 256, SRCCOPY);   //배경에 복사 및 출력
+        
+    BitBlt(hdc, 0, 0, 800, 600, MemDC, 0, 0, SRCCOPY);                      //배경을 화면으로 
+
+    // 에니메이션 -----------------------------------------------------------
+    if (frame < 2)
+    {
+        SelectObject(MemDC, MyBitmap3); 	    //비트맵 선택
+        TransparentBlt(hdc, x, y, 256, 256, MemDC, 0, 0, 256, 256, RGB(255, 255, 255));
+    }
+    else
+    {
+        SelectObject(MemDC, MyBitmap4); 	    //비트맵 선택
+        TransparentBlt(hdc, x, y, 256, 256, MemDC, 0, 0, 256, 256, RGB(255, 255, 255));
+    }
+
+    //SelectObject(MemDC, MyBitmap4); 	    //비트맵 선택
+    //static BLENDFUNCTION bf;
+    //bf.BlendOp = bf.BlendFlags = bf.AlphaFormat = 0;
+    //bf.SourceConstantAlpha = 128;
+    //AlphaBlend(hdc, 300, 0, 256, 256, MemDC, 0, 0, 256, 256, bf);
+
+    SelectObject(MemDC, OldBitmap);
+
+    DeleteDC(MemDC);
+    DeleteDC(MemDC1);
+    DeleteDC(MemDC2);
+
+    DeleteObject(MyBitmap1);
+    DeleteObject(MyBitmap2);
+    DeleteObject(MyBitmap3);
+    DeleteObject(MyBitmap4);
+}
+
+
+// TimerProc 
+void CALLBACK TimerProc(HWND hWnd, UINT uMsg, UINT idEvent, DWORD dwTime)
+{
+    //GetKeyState --------------------------------------------------------
+    //if (GetKeyState(VK_UP) < 0) y--;
+    //if (GetKeyState(VK_DOWN) < 0) y++;
+    //if (GetKeyState(VK_LEFT) < 0) x--;
+    //if (GetKeyState(VK_RIGHT) < 0) x++;
+
+    //GetAsyncKeyState ---------------------------------------------------
+    //0x0000        이전에 누른 적이 없고 호출 시점에도 눌려있지 않은 상태
+    //0x0001        이전에 누른 적이 있고 호출 시점에는 눌려있지 않은 상태
+    //0x8000        이전에 누른 적이 없고 호출 시점에는 눌려있는 상태    
+    //0x8001        이전에 누른 적이 있고 호출 시점에도 눌려있는 상태
+    //
+    //  한번 눌릴키가 쭉 눌려있는 상태인지 알고 싶다면 0x8000 을 사용.
+    //  키가 변했는지 알고 싶다면, & 연산을 사용 0x0001에 대해 검사.
+
+    if (GetAsyncKeyState(VK_LEFT) & 0x8000)     { x-= 3; }
+    if (GetAsyncKeyState(VK_RIGHT) & 0x8000)    { x+= 3; }
+    if (GetAsyncKeyState(VK_UP) & 0x8000)       { y-= 3; }
+    if (GetAsyncKeyState(VK_DOWN) & 0x8000)     { y+= 3; }
+
+    InvalidateRect(hWnd, NULL, TRUE);
+
+    frame++; frame %= 4;
+}
+
+
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 여기에 코드를 입력합니다.
+
+    // 전역 문자열을 초기화합니다.
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_PROJECT2, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 애플리케이션 초기화를 수행합니다:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    Init();
+
+    // 기본 메시지 루프입니다:
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_PROJECT2));
+    MSG msg;    
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    Release();
+
+    return (int) msg.wParam;
+}
+
+//  함수: MyRegisterClass()
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_PROJECT2));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_PROJECT2);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+    return RegisterClassExW(&wcex);
+}
+
+//   함수: InitInstance(HINSTANCE, int)
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+   if (!hWnd) { return FALSE; }
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+   return TRUE;
+}
+
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CREATE:
+        SetTimer(hWnd, 1, 1000, NULL);
+        SetTimer(hWnd, 2, 50, (TIMERPROC)TimerProc);
+        SendMessage(hWnd, WM_TIMER, 1, 0);   	// 타이머 아이디 1
+
+        GetClientRect(hWnd, &rt);
+        break;
+    case WM_TIMER:
+        {
+            switch (wParam) {
+            case 1:
+                {
+                    //GetLocalTime(&st);
+                    //_stprintf_s(sTime, _T("지금 시간은 %d:%d:%d입니다"), st.wHour, st.wMinute, st.wSecond);                    
+                    //InvalidateRect(hWnd, &rt, TRUE);	//InvalidateRect
+                }
+                break;
+            case 2:
+                //MessageBeep(0);
+                break;
+            }
+        }
+        break;
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 메뉴 선택을 구문 분석합니다:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+
+            Draw(hdc);
+
+            TextOut(hdc, 10, 10, sTime, _tcslen(sTime));
+
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        KillTimer(hWnd, 1);
+        KillTimer(hWnd, 2);
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
