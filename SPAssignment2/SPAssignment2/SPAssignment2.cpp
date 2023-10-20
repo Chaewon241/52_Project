006f45f564bb6839dea7f1eb63458386fed0e8a4
Chaewon <77910498+Chaewon241@users.noreply.github.com>
2023-10-20 15:25:13 +0900

    시스템 프로그래밍
    파일 입출력

---
+++ SPAssignment2/SPAssignment2/SPAssignment2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <fstream>
+#include <filesystem>
+
+using namespace std;
+using filesystem::directory_iterator;
+
+int countFilesWithExtension(const string& path, const string& extension) 
+{
+    int count = 0;
+    for (const auto& entry : directory_iterator(path)) 
+    {
+        if (entry.path().extension() == extension)
+        {
+            cout << entry.path() << endl;
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() 
+{
+    string path;
+    string extension;
+
+    cout << "디렉토리 경로를 입력하세요: ";
+    cin >> path;
+
+    cout << "확장자를 입력하세요 (예: .txt, .cpp, .jpg 등): ";
+    cin >> extension;
+
+    int fileCount = countFilesWithExtension(path, extension);
+    cout << "디렉토리 내 " << extension << " 확장자를 가진 파일 수: " << fileCount << endl;
+
+    return 0;
+}
---
+++ SysyemProgrammingAssignment/SysyemProgrammingAssignment/SysyemProgrammingAssignment.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <tchar.h>
+#include <locale>
+#include <codecvt>
+
+using namespace std;
+
+int main()
+{
+    // 콘솔 출력을 UTF-8로 설정
+    // 유니코드 문자열을 콘솔에 직접 출력하면 제대로 표시되지 않을 수 있기 때문
+    SetConsoleOutputCP(CP_UTF8); 
+
+    TCHAR test[] = _T("안녕하세요 HELLO");
+    char test2[] = "안녕하세요 HELLO";
+    char test3[] = u8"안녕하세요 HELLO";
+
+    // UTF-8로 인코딩된 문자열을 직접 유니코드로 변환
+    wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+    wstring unicode_string = converter.from_bytes(test3);
+
+    wcout.imbue(locale("ko_KR.UTF-8")); // 유니코드 출력을 위한 locale 설정
+    wcout << unicode_string << ", " << sizeof(test3) << endl;
+}
