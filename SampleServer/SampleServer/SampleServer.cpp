23941893fc1cf1f50e4e39b51dd24975cb2157dc
Chaewon <77910498+Chaewon241@users.noreply.github.com>
2024-04-29 23:02:06 +0900

    [서버프레임워크] RefCountable을 구현한 스마트포인터로 변경해봤다.

--- SampleServer/SampleServer/SampleServer.cpp
+++ SampleServer/SampleServer/SampleServer.cpp
@@ -8,47 +8,80 @@
 #include "ThreadManager.h"
 #include "RefCounting.h"
 
+
 class Wraight : public RefCountable
 {
 public:
 	int _hp = 150;
-	int _posX = 150;
-	int _posY = 150;
+	int _posX = 0;
+	int _posY = 0;
 };
+
+using WraightRef = TSharedPtr<Wraight>;
 
 class Missile : public RefCountable
 {
 public:
-	void SetTarget(Wraight* target)
+	void SetTarget(WraightRef target)
 	{
 		_target = target;
-		target->AddRef();
+		// 중간에 개입 가능
+		//target->AddRef();
 	}
 
-	void Update()
+	bool Update()
 	{
+		if (_target == nullptr)
+			return true;
+
 		int posX = _target->_posX;
 		int posY = _target->_posY;
+
+		// TODO : 쫓아간다
+
+		if (_target->_hp == 0)
+		{
+			//_target->ReleaseRef();
+			_target = nullptr;
+			return true;
+		}
+
+		return false;
 	}
 
-	Wraight* _target = nullptr;
+	WraightRef _target = nullptr;
 };
+
+using MissileRef = TSharedPtr<Missile>;
 
 int main()
 {
-	Wraight* wraight = new Wraight();
-	Missile* missile = new Missile();
+	WraightRef wraight(new Wraight());
+	wraight->ReleaseRef();
+	MissileRef missile(new Missile());
+	missile->ReleaseRef();
+
 	missile->SetTarget(wraight);
-	
-	// 레이스가 피격 당함.
+
+	// 레이스가 피격 당함
 	wraight->_hp = 0;
-	delete wraight;
+	//delete wraight;
+	//wraight->ReleaseRef();
+	wraight = nullptr;
 
 	while (true)
 	{
 		if (missile)
 		{
-			missile->Update();
+			if (missile->Update())
+			{
+				//missile->ReleaseRef();
+				missile = nullptr;
+			}
 		}
 	}
+
+	//missile->ReleaseRef();
+	missile = nullptr;
+	//delete missile;
 }
